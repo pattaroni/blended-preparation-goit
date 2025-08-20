8b92a83cbef418e3aa9debc1548f49ba5d748067
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-08-20 17:42:47 +0300

    add products render and fetch

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,9 +1,19 @@
-import axios from "axios"
-import { BASE_URL, ENDPOINTS } from "./constants"
+import axios from 'axios';
+import { BASE_URL, ENDPOINTS, PER_PAGE } from './constants';
 
-axios.defaults.baseURL = BASE_URL
+axios.defaults.baseURL = BASE_URL;
 
-export const fetchCategories = async() => {
-    const { data } = await axios.get(ENDPOINTS.CATEGORIES) ;
-    return data
-}
+export const fetchCategories = async () => {
+  const { data } = await axios.get(ENDPOINTS.CATEGORIES);
+  return data;
+};
+
+export const fetchProducts = async currentPage => {
+  const { data } = await axios.get(`${ENDPOINTS.PRODUCTS}`, {
+    params: {
+      limit: PER_PAGE,
+      skip: (currentPage - 1) * PER_PAGE,
+    },
+  });
+  return data;
+};
