e3cbf2db5ac335b4d289a58ec7f66ec4d603bfab
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-08-21 12:15:25 +0300

    Add product modal with details on click
    Implemented a modal that displays detailed product information when a product is clicked. Added event handlers for opening and closing the modal, fetching product details by ID, and rendering the product data in the modal. Updated references and API functions to support this feature.

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
   ulCategorEl: document.querySelector('.categories'),
   ulProductEl: document.querySelector('.products'),
+  modalEl: document.querySelector('.modal'),
+  modalCloseBtn: document.querySelector('.modal__close-btn'),
+  modalListEl: document.querySelector('.modal-product'),
 };
